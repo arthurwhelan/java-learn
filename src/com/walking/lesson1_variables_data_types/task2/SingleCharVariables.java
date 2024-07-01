b3773a15fefd4a2871a46c36591cc0528204fded
Arthur Whelan <dev.java.contact@gmail.com>
2024-07-01 22:38:50 +0300

    feat: сompleted lesson 1

---
+++ src/com/walking/lesson1_variables_data_types/task2/SingleCharVariables.java
@@ -0,0 +1,21 @@
+package com.walking.lesson1_variables_data_types.task2;
+
+// Используя
+// Одну переменную типа char.
+// Вывести свое имя в консоль.
+
+public class SingleCharVariables {
+    public static void main(String[] args) {
+        char singleChar = 'J';
+        System.out.print(singleChar);
+
+        singleChar = 'o';
+        System.out.print(singleChar);
+
+        singleChar = 'h';
+        System.out.print(singleChar);
+
+        singleChar = 'n';
+        System.out.print(singleChar);
+    }
+}
---
+++ src/com/walking/lesson1_variables_data_types/task2/StringVariables.java
@@ -0,0 +1,12 @@
+package com.walking.lesson1_variables_data_types.task2;
+
+// Используя
+// Переменную типа String;
+// Вывести свое имя в консоль.
+
+public class StringVariables {
+    public static void main(String[] args) {
+        String myName = "John";
+        System.out.println(myName);
+    }
+}
