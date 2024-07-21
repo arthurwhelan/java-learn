669b696984deb3b7205d840a6462467b355acb86
Arthur Whelan <dev.java.contact@gmail.com>
2024-07-21 16:22:57 +0300

    refactor: lesson 17 task 3

--- src/com/walking/lesson17_enum/task3/Main.java
+++ src/com/walking/lesson17_enum/task3/Main.java
@@ -3,6 +3,9 @@ package com.walking.lesson17_enum.task3;
 // Реализуйте задачу https://github.com/KFalcon2022/practical-tasks/tree/master/src/com/walking/lesson14_polymorphism/task1
 // через enum. Пусть EquilateralTriangle и Square будут значениями enum's EquilateralShape
 
+import com.walking.lesson14_polymorphism_overriding_method.task1.model.EquilateralFigure;
+import com.walking.lesson14_polymorphism_overriding_method.task1.model.EquilateralTriangle;
+import com.walking.lesson14_polymorphism_overriding_method.task1.model.Square;
 import com.walking.lesson17_enum.task3.shape.EquilateralShapeType;
 
 import java.util.Scanner;
@@ -14,24 +17,56 @@ public class Main {
         System.out.println("Enter side length: ");
         int sideLength = scanner.nextInt();
 
+        if (!validateSideLength(sideLength)) {
+            return;
+        }
+
         System.out.println("Select a shape:\n1. square\n2. triangle");
         int selectedShape = scanner.nextInt();
 
+        if (!validateSelectedShape(selectedShape)) {
+            return;
+        }
+
         scanner.close();
 
-        if (!validation(sideLength, selectedShape)) {
-            System.out.println("Invalid side length or unknown shape");
-        } else {
-            String printShape = selectedShape == 1
-                    ? EquilateralShapeType.SQUARE.createShape(sideLength)
-                    : EquilateralShapeType.EQUILATERAL_TRIANGLE.createShape(sideLength);
-
-            System.out.println(printShape);
-        }
+        String shapeString = createEquilateralFigure(sideLength, selectedShape);
+        System.out.println(shapeString);
     }
 
-    private static boolean validation(int sideLength, int selectedShape) {
-        return selectedShape > 0 && selectedShape < 3 && sideLength > 0;
+    private static String createEquilateralFigure(int sideLength, int selectedShape) {
+       EquilateralShapeType shapeType;
+
+        switch (selectedShape) {
+            case 1:
+                shapeType = EquilateralShapeType.SQUARE;
+                break;
+            case 2:
+                shapeType = EquilateralShapeType.EQUILATERAL_TRIANGLE;
+                break;
+            default:
+                return "Unknown shape";
+        }
+
+        return shapeType.createShape(sideLength);
+    }
+
+    private static boolean validateSideLength(int sideLength) {
+        if (sideLength < 1) {
+            System.out.println("Invalid side length. Side length must be greater than 0.");
+            return false;
+        }
+
+        return true;
+    }
+
+    private static boolean validateSelectedShape(int selectedShape) {
+        if (selectedShape < 1 || selectedShape > 2) {
+            System.out.println("Invalid selected shape.");
+            return false;
+        }
+
+        return true;
     }
 }
 
